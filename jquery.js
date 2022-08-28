cd5d576d35ed51d8e687daf5d4c539404f04e842
JelmerKorten <jelmer.korten@gmail.com>
2022-08-28 16:11:48 +0400

    changed navbar load method

--- jquery.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("remoteNavbar").innerHTML +=
-    '<div class="scrollmenu">\
-        <a href="/index.html">Home</a>\
-        <a href="#news">News</a>\
-        <a href="/recipes.html">Recipes</a>\
-        <a href="/contact.html">Contact</a>\
-        <a href="/socials.html">Socials</a>\
-        <a style="float:right" href="/about.html">About</a>\
-    </div>';
---
+++ script.js
@@ -0,0 +1,6 @@
+const nav = document.querySelector('.navbar')
+fetch('/navbar.html')
+.then(res=>res.text())
+.then(data=>{
+    nav.innerHTML=data
+})
