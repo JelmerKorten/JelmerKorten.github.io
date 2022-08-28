00b33d45524ce754e4e8238c18dc0dc8262ff6ee
JelmerKorten <jelmer.korten@gmail.com>
2022-08-28 14:29:23 +0400

    added .js and summer pasta salad test.

---
+++ jquery.js
@@ -0,0 +1,9 @@
+document.getElementById("remoteNavbar").innerHTML +=
+    '<div class="scrollmenu">\
+        <a href="/index.html">Home</a>\
+        <a href="#news">News</a>\
+        <a href="/recipes.html">Recipes</a>\
+        <a href="/contact.html">Contact</a>\
+        <a href="/socials.html">Socials</a>\
+        <a style="float:right" href="/about.html">About</a>\
+    </div>';
